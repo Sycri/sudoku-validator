3fa7b5446d8e9f1833270a528689e59fe5c00e7c
Sycri <39947226+Sycri@users.noreply.github.com>
2022-03-31 21:10:35 +0300

    Add functions to check all rows, columns, squares

--- src/validator.js
+++ src/validator.js
@@ -31,7 +31,6 @@ class Validator {
         square.push(arr[squarePos + i + j])
       }
     }
-    console.log(square)
     return square
   }
 
@@ -40,20 +39,62 @@ class Validator {
 		return sudoku.split(/\D+/, 81).map(Number)
 	}
 
+  // Zeroes represent empty space, so they should be removed from an array for unique value checks
   filterZeroesFromArray(arr) {
     return arr.filter((value) => value != 0)
   }
 
-	uniqueValuesInArray(arr) {
-		return new Set(arr).size == arr.length
-	}
+  uniqueValuesInArray(arr) {
+    return new Set(arr).size == arr.length
+  }
 
   isComplete(arr) {
     return arr.some((value) => value == 0) == false
   }
 
+  checkAllRows(arr) {
+    for (let i = 0; i < 9; ++i) {
+      let row = this.getRow(arr, i)
+      row = this.filterZeroesFromArray(row)
+
+      if (!this.uniqueValuesInArray(row))
+        return false
+    }
+    return true
+  }
+
+  checkAllColumns(arr) {
+    for (let i = 0; i < 9; ++i) {
+      let col = this.getCol(arr, i)
+      col = this.filterZeroesFromArray(col)
+
+      if (!this.uniqueValuesInArray(col))
+        return false
+    }
+    return true
+  }
+
+  checkAllSquares(arr) {
+    for (let i = 0; i < 9; ++i) {
+      let square = this.getSquare(arr, i)
+      square = this.filterZeroesFromArray(square)
+
+      if (!this.uniqueValuesInArray(square))
+        return false
+    }
+    return true
+  }
+
   validate(sudoku) {
-    // Your code here
+    let arr = this.fromString(sudoku)
+
+    if (!this.checkAllRows(arr) || !this.checkAllColumns(arr) || !this.checkAllSquares(arr))
+      return "Sudoku is invalid."
+
+    if (this.isComplete(arr))
+      return "Sudoku is valid."
+    else
+      return "Sudoku is valid but incomplete."
   }
 }
 
